e3a6093a62643b91c1429d4fcbf3925d08b4c13c
nilpatel <nilpatel62@gmail.com>
2024-04-14 22:01:50 +0530

    initial commit.

---
+++ elastic_apis/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ElasticApisConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'elastic_apis'
---
+++ elastic_apis/views.py
@@ -0,0 +1,88 @@
+from rest_framework.views import APIView
+from django.http import HttpResponse, JsonResponse
+from elastic_search_api_new.settings import es_url
+
+
+# Create your views here.
+class ElasticData(APIView):
+    """
+        API view for fetching data from Elasticsearch based on a search query.
+
+        The view's `get` method accepts a `search` term, a `skip` parameter for pagination offset,
+        and a `limit` parameter for the number of results to return. It builds an Elasticsearch query
+        based on whether the `search` parameter is provided. If `search` is not empty, it adds a
+        `match_phrase_prefix` filter to narrow the results to those where the 'hostname' field starts
+        with the search term. The method then constructs an Elasticsearch query using the boolean `must`
+        condition when there are search terms and handles pagination with `skip` and `limit`. The results
+        are filtered to only include the ID and host fields from the source.
+
+        If the Elasticsearch query returns results, the method packages these into a JSON response
+        along with a success message. If no results are found, it returns a JSON response with an
+        error message and a 404 status. Any exceptions in the process are caught, and an error message
+        with a 500 status code is returned along with the error details.
+
+        Attributes:
+            es_url (Elasticsearch): The Elasticsearch connection used for making search queries.
+
+        Methods:
+            get(self, request, skip, limit, search): Processes the GET request to search data in Elasticsearch.
+    """
+    def get(self, request, skip, limit, search):
+        try:
+            must_query = []
+            if search != "":
+                must_query.append({"match_phrase_prefix": {"hostname": search}})
+
+            if len(match_phrase_prefix) == 0:
+                search_query = {
+                    "query": {
+                        "bool": {
+                        }
+                    },
+                    "size": skip,
+                    "from": limit,
+                }
+            else:
+                search_query = {
+                    "query": {
+                        "bool": {
+                            "must": must_query,
+                            "minimum_should_match": 1,
+                            "boost": 1.0,
+                        }
+                    },
+                    "size": skip,
+                    "from": limit,
+                }
+            res_filter_parameters = es_url.search(
+                index="filebeat-*",
+                body=search_query,
+                filter_path=[
+                    "hits._id",
+                    "hits._source.host",
+                ],
+            )
+
+            if len(res_filter_parameters) == 0:
+                response = {"data": [], "message": "No Data Found"}
+                return JsonResponse(response, safe=False, status=404)
+            else:
+                response_data = []
+                for _res in res_filter_parameters['hits']:
+                    response_data.append(
+                        {
+                            "id": _res['_id'],
+                            "host": _res['_source']['host']
+                        }
+                    )
+                response = {"data": response_data, "message": "Data Found"}
+                return JsonResponse(response, safe=False, status=200)
+
+
+
+        except Exception as ex:
+            print("Error on line {}".format(sys.exc_info()[-1].tb_lineno), type(ex).__name__, ex)
+            error = {
+                "message": "something went wrong"
+            }
+            return JsonResponse(error, safe=False, status=500)
